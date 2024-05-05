88a5645d1e40d8dde4049d0d7d522f92e9af38d6
AuChww <chanawut.w@ku.th>
2024-05-06 03:51:06 +0700

    add migration

---
+++ 4.API/Program.cs
@@ -0,0 +1,65 @@
+using _2.Core.Extensions;
+using _3.Infra;
+using _3.Infra.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region Configure IOption Pattern
+
+// builder.Services.ConfigureInfraOptions();
+
+#endregion
+
+#region Configure DI Container - Service Lifetimes - Infra
+
+builder.Services.AddInfraDependencyInjection(builder.Configuration);
+
+#endregion
+
+#region Configure DI Container - Service Lifetimes - Business Services
+
+builder.Services.AddCoreDependencyInjection();
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await SeedDatabase();
+
+app.Run();
+
+async Task SeedDatabase()
+{
+  using (var scope = app.Services.CreateScope())
+  {
+    var dbcontext = 
+      scope.ServiceProvider.GetRequiredService<DataContext>();
+    
+    // Run migration scripts
+    await dbcontext.Database.MigrateAsync();
+
+    // Seed data to the project
+    await _3.Infra.Seed.SeedData(dbcontext);
+  }
+}
